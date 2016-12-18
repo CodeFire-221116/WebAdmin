03c396ad5a0c8140bc48025d1d4fef9ee0ada080
krismars <kristipydel@gmail.com>
2016-12-18 13:07:13 +0200

    Showing of online users added.

--- src/main/java/ua/com/codefire/cms/listener/UsersSessionListener.java
+++ src/main/java/ua/com/codefire/cms/listener/UsersSessionListener.java
@@ -1,33 +1,43 @@
 package ua.com.codefire.cms.listener;
 
 import ua.com.codefire.cms.model.AttributeNames;
+import ua.com.codefire.cms.model.Fields;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.servlet.ServletContext;
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import javax.servlet.http.HttpSessionAttributeListener;
-import javax.servlet.http.HttpSessionBindingEvent;
-import javax.servlet.http.HttpSessionEvent;
-import javax.servlet.http.HttpSessionListener;
+import javax.servlet.http.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Created by human on 12/6/16.
+ * Created by human on 12/6/16
  */
+
 @WebListener
 public class UsersSessionListener implements HttpSessionListener, HttpSessionAttributeListener {
 
+    private Map<String, String> users = new HashMap<>();
+
+    private ServletContext context;
+
     @Override
     public void sessionCreated(HttpSessionEvent se) {
-        // USER CONNECT
+
+        context = se.getSession().getServletContext();
+
+        HttpSession session = se.getSession();
+        users.put(session.getId(), Fields.USER_ANONYMOUS);
+
+        context.setAttribute(Fields.USERS, users);
     }
 
     @Override
     public void attributeAdded(HttpSessionBindingEvent event) {
+
         if (event.getName().equals(AttributeNames.SESSION_USERNAME)) {
+
             String username = event.getValue().toString();
+            users.put(event.getSession().getId(), username);
         }
     }
 
@@ -43,6 +53,10 @@ public class UsersSessionListener implements HttpSessionListener, HttpSessionAtt
 
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
-        // USER DISCONNECT
+
+        HttpSession session = se.getSession();
+        users.remove(session.getId());
+
+        context.setAttribute(Fields.USERS, users);
     }
 }
