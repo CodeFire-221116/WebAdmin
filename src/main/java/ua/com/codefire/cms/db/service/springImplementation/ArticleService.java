fd9b1c41f05a1fbaaf6c3ffda21edd91b981ecda
Alex Melnyk <melnyk@codefire.com.ua>
2016-12-27 21:09:30 +0200

    Fix service

--- src/main/java/ua/com/codefire/cms/db/service/springImplementation/ArticleService.java
+++ src/main/java/ua/com/codefire/cms/db/service/springImplementation/ArticleService.java
@@ -8,8 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.SQLWarningException;
 import org.springframework.stereotype.Service;
 import ua.com.codefire.cms.db.entity.ArticleEntity;
-import ua.com.codefire.cms.db.service.abstraction.IArticleService;
 import ua.com.codefire.cms.db.repo.ArticleEntityRepository;
+import ua.com.codefire.cms.db.service.abstraction.IArticleService;
 
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
@@ -22,17 +22,16 @@ import java.util.logging.Logger;
  * An implementation of entity-specific Service Interface.
  * The object of this class needs to be put in the IArticleService variable in case of need in entity-specific
  * methods and in the ICommonService(ArticleEntity) variable in case of need in CRUD operations.
+ *
  * @author ankys
  */
 @Service
 public class ArticleService implements IArticleService {
-    private ArticleEntityRepository articleRepo;
+
     private static final Logger LOGGER = Logger.getLogger(ArticleEntityRepository.class.getName());
 
     @Autowired
-    public ArticleService(ArticleEntityRepository articleEntityRepository) {
-        this.articleRepo = articleEntityRepository;
-    }
+    private ArticleEntityRepository articleRepo;
 
     @Override
     public Long create(ArticleEntity objToCreate) {
@@ -43,7 +42,7 @@ public class ArticleService implements IArticleService {
             LOGGER.log(Level.SEVERE, "Spring-specific exception", ex);
         } catch (EntityExistsException ex) {
             LOGGER.log(Level.SEVERE, "Such article already exists.", ex);
-        } catch (PersistenceException ex){
+        } catch (PersistenceException ex) {
             LOGGER.log(Level.SEVERE, "Problems with database, while creating new article.", ex);
         } catch (Exception ex) {
             LOGGER.log(Level.SEVERE, "Unexpected exception", ex);
@@ -63,7 +62,7 @@ public class ArticleService implements IArticleService {
             return true;
         } catch (EntityNotFoundException ex) {
             LOGGER.log(Level.SEVERE, "No article found by such id.", ex);
-        } catch (PersistenceException ex){
+        } catch (PersistenceException ex) {
             LOGGER.log(Level.SEVERE, "Problems with database, while deleting article.", ex);
         } catch (Exception ex) {
             LOGGER.log(Level.SEVERE, "Unexpected exception, while deleting article.", ex);
@@ -80,7 +79,7 @@ public class ArticleService implements IArticleService {
             LOGGER.log(Level.SEVERE, "No such article found.", ex);
         } catch (SQLWarningException ex) {
             LOGGER.log(Level.SEVERE, "Spring-specific exception", ex);
-        } catch (PersistenceException ex){
+        } catch (PersistenceException ex) {
             LOGGER.log(Level.SEVERE, "Problems with database, while updating article.", ex);
         } catch (Exception ex) {
             LOGGER.log(Level.SEVERE, "Unexpected exception", ex);
